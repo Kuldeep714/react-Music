705f9090c1c72cf32bd184b1070162d7185c57c8
kuldeep singh <kuldeepjassal178@gmail.com>
2025-04-02 22:37:26 +0530

    Update index.js

--- music/src/index.js
+++ music/src/index.js
@@ -1,1 +1,19 @@
-https://flylib.com/books/en/1.102.1.15/1/
+
+import React from "react";
+import ReactDOM from "react-dom/client"; 
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+const root = ReactDOM.createRoot(document.getElementById("root")); 
+root.render(
+
+<React.StrictMode>
+    <BrowserRouter> 
+    <App />
+    </BrowserRouter> 
+</React.StrictMode>
+
+);
+
+reportWebVitals();
